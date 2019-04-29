bd35e955471d38b7409a152552bcc98293c00815
Mohammad Kefah <mo@edraj.io>
2019-04-29 10:47:51 +0300

    Tests: Added test layout for SkipNode.searchNum method

--- src/test/SkipNodeTest.java
+++ src/test/SkipNodeTest.java
@@ -1,5 +1,48 @@
 public class SkipNodeTest {
     public void SearchNumIDTest() {
+
+
+        // TODO: populate lookupTable
+        NodeInfo[][] lookupTable = new NodeInfo[][] {
+            { new NodeInfo(), new NodeInfo() },
+                { new NodeInfo(), new NodeInfo() },
+                { new NodeInfo(), new NodeInfo() },
+                { new NodeInfo(), new NodeInfo() },
+                { new NodeInfo(), new NodeInfo() },
+                { new NodeInfo(), new NodeInfo() },
+        };
+
+        class TestCase {
+            public int     numID;
+            public int     level;
+            public String  output;
+
+            public TestCase(int numID, int level, String output) {
+                this.numID = numID;
+                this.level = level;
+                this.output = output;
+            }
+        }
+
+
+        // Initialize node with the given lookupTable
         SkipNode node = new SkipNode();
+
+
+        // TODO: populate test table
+        TestCase tt[] = new TestCase[]{
+                new TestCase(),
+                new TestCase(),
+                new TestCase(),
+                new TestCase(),
+                new TestCase(),
+                new TestCase(),
+                new TestCase(),
+        };
+
+
+        for(TestCase tc : tt) {
+            Assert(tc.ouput, node.searchNum(tc.numID, tc.level));
+        }
     }
 }
